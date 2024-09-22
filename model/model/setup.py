537641688b5e634081e09f77af9106d5c014febc
Ubuntu <ubuntu@ip-172-31-94-85.ec2.internal>
2024-09-22 18:39:27 +0000

    first commit Committer: Ubuntu <ubuntu@ip-172-31-94-85.ec2.internal>

---
+++ model/model/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='to check availability of land for sale and aquisition around power stations in the US',
+    author='HD',
+    license='',
+)
